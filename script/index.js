5e3e138bf230b5692b866002779ac026141468b9
alattalatta <urty5656@gmail.com>
2022-11-01 21:54:31 +0900

    1.4.3534: CLI game clear/deploy/pull commands

--- script/index.js
+++ script/index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-import fs from 'node:fs'
 import path from 'node:path'
 import readline from 'node:readline'
 
 import { execa } from 'execa'
+import fs from 'fs-extra'
+import { getGamePath } from 'steam-game-path'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+
+const AVAILABLE_DLCS = ['Core', 'Royalty', 'Ideology', 'Biotech']
+const AVAILABLE_TRANSLATIONS = ['Core', 'Royalty', 'Ideology']
 
 yargs(hideBin(process.argv))
   .scriptName('cli')
@@ -45,10 +49,74 @@ yargs(hideBin(process.argv))
     }
   )
   .command(
+    'clear',
+    'clear all language files from RimWorld',
+    (ins) => ins.help(),
+    () => {
+      const gamePath = path.join(getGamePath(294100).game.path, 'Data')
+
+      return Promise.all(
+        AVAILABLE_DLCS.map((dlc) => {
+          const dlcLangPath = path.join(gamePath, dlc, 'Languages')
+
+          // get all tar files
+          const tarFiles = fs.readdirSync(dlcLangPath).filter((file) => file.endsWith('.tar'))
+
+          // remove them all
+          return Promise.all(
+            tarFiles.map((tarFile) => fs.remove(path.join(dlcLangPath, tarFile)))
+          )
+        })
+      )
+    }
+  )
+  .command(
     'deploy',
     'deploy language data to RimWorld installation path',
     (ins) => ins.help(),
-    async () => {}
+    () => {
+      const gamePath = path.join(getGamePath(294100).game.path, 'Data')
+
+      return Promise.all(
+        AVAILABLE_TRANSLATIONS.map(async (dlc) => {
+          const sourcePath = path.join(process.cwd(), dlc)
+          const dlcLangPath = path.join(gamePath, dlc, 'Languages/Korean (한국어)')
+
+          await fs.remove(dlcLangPath)
+          await fs.ensureDir(dlcLangPath)
+
+          // get all files and directories of sourcePath
+          const entries = fs.readdirSync(sourcePath)
+
+          // copy them under dlcLangPath
+          return Promise.all(entries.map((name) => fs.copy(path.join(sourcePath, name), path.join(dlcLangPath, name))))
+        })
+      )
+    }
+  )
+  .command(
+    'pull',
+    'pull language data from RimWorld installation path',
+    (ins) => ins.help(),
+    () => {
+      const gamePath = path.join(getGamePath(294100).game.path, 'Data')
+
+      return Promise.all(
+        AVAILABLE_TRANSLATIONS.map(async (dlc) => {
+          const sourcePath = path.join(gamePath, dlc, 'Languages/Korean (한국어)')
+          const translationPath = path.join(process.cwd(), dlc)
+
+          await fs.remove(translationPath)
+          await fs.ensureDir(translationPath)
+
+          // get all files and directories of sourcePath
+          const entries = fs.readdirSync(sourcePath)
+
+          // copy them under translationPath
+          return Promise.all(entries.map((name) => fs.copy(path.join(sourcePath, name), path.join(translationPath, name))))
+        }
+      ))
+    }
   )
   .command(
     'worker',
